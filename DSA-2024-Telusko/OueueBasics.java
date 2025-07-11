0561f7817db4cf36462ab25eb7e000ffc5da37f3
diwahar-m <msdiwahar5@gmail.com>
2025-07-11 15:51:20 +0530

    added pre order & post order inbinary tree

--- DSA-2024-Telusko/OueueBasics.java
+++ DSA-2024-Telusko/OueueBasics.java
@@ -3,7 +3,11 @@ public class OueueBasics {
         Queue queue = new Queue();
 
         queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(5);
+        queue.enqueue(99);
         queue.show(); 
+        queue.peek();
         
         System.out.println(queue.dequeue());
         queue.show();
